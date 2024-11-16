7cb8f30483f43a2f6774e291097d6e8cef00c937
NP <npicciano79@gmail.com>
2024-11-15 22:19:41 -0500

    updated pass encrypt

--- password_encrypt.py
+++ password_encrypt.py
@@ -1,36 +1,57 @@
 import hashlib
+import bcrypt 
+from passlib.hash import pbkdf2_sha256
+    
 
-#function create hashed password 
-#takes user password and hashes 
-def f_createHashedPassword (lv_password):
-    print('lv_pass: '+lv_password)
-
+#create password salt
+#def f_create_Password_Salt ():
+    
+    
+    
+    
+    
         
 #lv_hashed_pass = hashlib.sha256
 
 #class hashUserPass created to contain objects used to hash and store password 
 class hashedPassword:
     #NPSUPASS stores index, hash, salt, user
-    def __init__(self , lv_pass , lv_hash , lv_salt ,  lv_user):
-        self.sv_pass            = lv_pass
+    def __init__(self , lv_hash , lv_salt ,  lv_user):
         self.sv_hash            = lv_hash
         self.sv_salt            = lv_salt
         self.sv_user            = lv_user
-        
-        print(f"password: {self.sv_pass} hash: {self.sv_hash} salt: {self.sv_salt} user: {self.sv_user}")
-        
-    
-    #call function to hash password
-    self.hash = f_createHashedPassword (self.sv_pass)
 
+
+
+
+#takes user password and generates hash
+#adds salt into hashed password
+def f_create_Password_Hash (lv_password):
+    #print('lv_pass: '+lv_password)
+    return pbkdf2_sha256.hash(lv_password)
+    
+   
+   
 
 
 
 
 #initialize program
 if __name__ == "__main__":
+    
+    #retrieve user password as lv_password    
+    lv_password = input('Enter your password: ')
+    
+    #passes lv_password to has function and returns
+    #sv hash as hash and salt
+    sv_hashedPass_Salt = f_create_Password_Hash (lv_password)
+
+    lv_salt,lv_hash = sv_hashedPass_Salt.split("-")[0], sv_hashedPass_Salt.split("-")[1]
+    
+    print ('hash: '+lv_hash+" salt: "+lv_salt)
+    
     #call call hashedPassword 
-    pv_pass = hashedPassword (1234 , 'pfv' , 0000, 'Nick')  
+    #pv_pass = hashedPassword (1234 , 'pfv' , 0000, 'Nick')  
     
     
 
