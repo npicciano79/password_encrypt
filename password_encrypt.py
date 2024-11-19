c62e318abbaff85a5721711be48f9ab41994c2e6
NP <npicciano79@gmail.com>
2024-11-18 19:06:53 -0500

    password encrypt

--- password_encrypt.py
+++ password_encrypt.py
@@ -1,77 +1,51 @@
 import hashlib
 import bcrypt 
+#import password_encrypt.db.pass_encrypt_db
 from passlib.hash import pbkdf2_sha256
-    
-
-#create password salt
-#def f_create_Password_Salt ():
-    
-    
-    
-    
-    
-        
-#lv_hashed_pass = hashlib.sha256
-
-#class hashUserPass created to contain objects used to hash and store password 
-class hashedPassword:
-    #NPSUPASS stores index, hash, salt, user
-    def __init__(self , lv_hash , lv_salt ,  lv_user):
-        self.sv_hash            = lv_hash
-        self.sv_salt            = lv_salt
-        self.sv_user            = lv_user
-
-
-
 
 #takes user password and generates hash
 #adds salt into hashed password
-def f_create_Hash (lv_pass_user_arr):
+def f_create_Hash (lv_username_password):
     #print('lv_pass: '+lv_password)
-    for i in range(1):
-        return pbkdf2_sha256.hash(lv_pass_user_arr[i])
-    
+    #lv_temp_pass_user_arr = []
+        return pbkdf2_sha256.hash(lv_username_password)
+  
+  
+  
+#use imported package to insert user pass into DB
+def f_database_Insert (pv_username,
+                       pv_pass_hash, 
+                       pv_pass_salt):
+    print(obj_user_pass)
+  
+  
    
    
-
-
-
-
 #initialize program
 if __name__ == "__main__":
     
     #retrieve username and password from user and stored in array 
-    lv_pass_user_arr = [] 
+    la_pass_user_arr = [] 
     lv_username = input('Enter your username: ')
-    lv_pass_user_arr.append(lv_username)
+    #la_pass_user_arr.append(lv_username)
     lv_password = input('Enter your password: ')
-    lv_pass_user_arr.append(lv_password)
-    print(lv_pass_user_arr)
-    #passes lv_password to has function and returns
-    #sv hash as hash and salt
-    sv_hashedPass_Salt = f_create_Hash (lv_pass_user_arr)
-
-    print('test array: '+sv_hashedPass_Salt)
-    '''
-    lv_salt,lv_hash = sv_hashedPass_Salt.split("-")[0], sv_hashedPass_Salt.split("-")[1]
+    #la_pass_user_arr.append(lv_password)
+    #user entered password and user name
+    #print('username password array: '+la_pass_user_arr)
     
-    print ('hash: '+lv_hash+" salt: "+lv_salt)
-    '''
-    #call call hashedPassword 
-    #pv_pass = hashedPassword (1234 , 'pfv' , 0000, 'Nick')  
+    #passes lv_password into hash function to return hash and salt pass
+    lv_password = f_create_Hash (lv_password)
+    
+    lv_pass_hash,lv_pass_salt = lv_password.split("-")[1],lv_password.split("-")[0]
+    
+    print('salt: '+lv_pass_salt+' hash: '+lv_pass_hash)
+ 
+    #creates user object calling encryptedUser class
+    #obj_user_pass = encryptedUser (lv_username , lv_pass_hash , lv_pass_salt)
     
     
-
-"""
-lv_hashed_pass.update('''pass to hash''')
-#Procedure hashPass
-def hashPass (self, 
-              lv_password):
-    
-    def __init__ (self):
-        
-    
-    def init self 
-    
-    """
+    #pass obj_user_pass to pass_encrypt_db to insert into database
+    f_database_Insert (lv_username, 
+                       lv_pass_hash, 
+                       lv_pass_salt)
     
